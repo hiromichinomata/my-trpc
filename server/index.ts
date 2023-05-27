20b7f6ccaaf038d92d5e5ea8c7de12806caab77b
Hiromichi NOMATA <git@hiromichinomata.com>
2023-05-27 18:54:56 +0900

    initial commit

---
+++ server/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res) => res.send('hello'));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
